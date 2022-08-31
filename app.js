7807373c22cdd77a206c31577d565d1323112c7f
kimry02 <ryan02.kim@gmail.com>
2022-08-30 20:23:47 -0400

    Redesigned UI and features for interactions.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildPresences], partials: [Partials.Message] });
 const axios = require('axios').default;
+const backtick = "```";
 
 client.once('ready', () => {
     console.log('Bot is ready!');
 })
 
 client.on('interactionCreate', async interaction => {
-    console.log(interaction);
 
     if(!interaction.isChatInputCommand()) return;
     const { commandName } = interaction;
@@ -31,15 +31,52 @@ client.on('messageCreate', async message => {
             summID = args[0];
         }
         try {
-            const PUUID = await getPUUID(summID);
+            const [PUUID, iconID] = await getPUUID(summID);
             if(PUUID === null) {
                 throw error;
             }
-            const matchID = await getLoss(PUUID);
+            const matchID = await getMID(PUUID);
             const res = await getMatch(matchID[0], PUUID);
-            message.reply(`${summID} went ${res.k}/${res.d}/${res.a} on ${res.champ}.`);
+            const arr = summID.split(' ');
+            let urlSummID = summID;
+            if(arr.length > 1) {
+                urlSummID = arr.join("%20");
+            }
+            if(res.won) {
+                const winEmbed = new EmbedBuilder()
+                .setColor(0x00FF00)
+                .setTitle(`Recent Ranked Game`)
+                .setURL(`https://na.op.gg/summoners/na/${urlSummID}/`)
+                .setAuthor({ name: `${summID}`, iconURL: `https://ddragon.leagueoflegends.com/cdn/12.16.1/img/profileicon/${iconID}.png` })
+                .setDescription('Click to follow OP.GG link')
+                .setThumbnail(`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${res.champid}.png`)
+                .addFields(
+                    { name: 'VICTORY', value: `${res.k}/${res.d}/${res.a}` },
+                    { name: 'CS', value: res.minionKill },
+                    { name: 'Duration', value: `${minutes}:${seconds}`}
+                );            
+                message.reply({ embeds: [winEmbed] });
+            } else {
+                const minutes = Math.floor((res.matchDuration) / 60);
+                const seconds = ((res.matchDuration) - (minutes * 60));
+                const lossEmbed = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setTitle(`Recent Ranked Game`)
+                .setURL(`https://na.op.gg/summoners/na/${urlSummID}/`)
+                .setAuthor({ name: `${summID}`, iconURL: `https://ddragon.leagueoflegends.com/cdn/12.16.1/img/profileicon/${iconID}.png` })
+                .setDescription('Click to follow OP.GG link')
+                .setThumbnail(`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${res.champid}.png`)
+                .addFields(
+                    { name: 'DEFEAT', value: `${res.k}/${res.d}/${res.a}`, inline: true },
+                    { name: 'CS', value: `${res.minionKill}`, inline: true },
+                    { name: 'Duration', value: `${minutes}:${seconds >= 10 ? seconds : '0' + seconds}`, inline: true}
+                );
+            
+                message.reply({ embeds: [lossEmbed] });
+            }
         } catch (error) {
-            message.reply(`Summoner ${summID} not found...`);
+            console.log(error);
+            message.reply(`${backtick}Summoner ${summID} not found...${backtick}`);
         }
     }
 })
@@ -47,13 +84,13 @@ client.on('messageCreate', async message => {
 async function getPUUID(summonerString) {
     try {
         const res = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerString}?api_key=${process.env.RG_TOKEN}`);
-        return res.data.puuid;
+        return [res.data.puuid, res.data.profileIconId];
     } catch (error) {
         return null;
     }
 }
 
-async function getLoss(summonerPUUID) {
+async function getMID(summonerPUUID) {
     return await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${summonerPUUID}/ids`, {
         params: {
             type: 'ranked',
@@ -97,7 +134,11 @@ async function getMatch(matchID, summonerPUUID) {
                 k: summoner.kills,
                 d: summoner.deaths,
                 a: summoner.assists,
-                champ: summoner.championName
+                champ: summoner.championName,
+                won: summoner.win,
+                champid: summoner.championId,
+                minionKill: summoner.totalMinionsKilled,
+                matchDuration: response.data.info.gameDuration
             };
             return responseData;
         }).catch(function (error) {
